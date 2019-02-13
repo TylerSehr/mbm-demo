1a1a7379f0825cce460a0589e5be32bdd7e92985
TylerPrivateGithub <eddywilkins34@gmail.com>
2019-02-12 19:31:22 -0600

    build

--- server/routes/p2p.router.js
+++ server/routes/p2p.router.js
@@ -8,7 +8,7 @@ const options = {
 }
 
 const PeerServer = require('peer').PeerServer;
-const server = PeerServer({port: 9000, path: '/'});
+const server = PeerServer({port: 9000, path: '/peerjs'});
 
 let peers = []
 
